b0056f36ba9fd5920e8d3edc07ac8cbe72e1fe84
achitsak028 <patong028@gmail.com>
2023-01-13 17:02:12 +0700

    lab14_1 complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,25 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[])
+    {
+         B[0]=0;
+         double sum=0,sum1=1,sum2=0,max=0,min=100;
+        for(int i=0;i < N;i++){
+            B[0]+=A[i]; 
+            sum+=A[i]*A[i];
+            sum1*=A[i];
+            sum2+=1.0/A[i];
+            if(max<A[i]){max=A[i];}
+            if(min>A[i]){min=A[i];}
+
+
+        }
+        B[0]=B[0]/N;
+        B[1]=sqrt((sum/N)-(B[0]*B[0]));
+        B[2]=pow(sum1,1.0/N);
+        B[3]=N/sum2;
+        B[4]=max;
+        B[5]=min;
+
+    }
